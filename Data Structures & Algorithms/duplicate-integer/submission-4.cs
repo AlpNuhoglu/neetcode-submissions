d8c8b982eefd01f90173840f6500430eb0bbd649
Alp <alpnuhoglu2@gmail.com>
2026-09-03 11:06:22 +0300

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        int n = nums.Length; 
+
+        Dictionary<int, int> myDic = new Dictionary<int, int>(); 
+
+        for (int i = 0; i < n; i++) 
+        {
+            if(myDic.ContainsValue(nums[i])) return true; 
+
+            myDic.Add(i, nums[i]); 
+
+        } 
+        return false; 
+
+    }
+}
