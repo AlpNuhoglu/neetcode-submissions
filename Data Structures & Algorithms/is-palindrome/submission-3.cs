68cdbcb826acb1d6c6850726cbe1d5e159831d4f
Alp <alpnuhoglu2@gmail.com>
2026-09-11 12:48:26 +0300

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        if (s == null) return false; 
+
+        int start = 0; 
+        int end = s.Length - 1; 
+
+        while (start < end) {
+    
+            while (start < end && !char.IsLetterOrDigit(s[start])) start++; 
+
+            while (start < end && !char.IsLetterOrDigit(s[end])) end--; 
+
+            if (char.ToLower(s[start]) != char.ToLower(s[end])) {
+                return false; 
+            }
+
+
+            start++; 
+            end--; 
+        }
+
+        return true; 
+    }
+
+}
